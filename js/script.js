ea89662c3deeef5b2fe85dc79e55b1efd9dde0de
BritishPandaChick <sbartley1080@gmail.com>
2019-03-28 15:34:10 -0400

    Add js folder. Move js file to new folder and change link src in index.html

---
+++ js/script.js
@@ -0,0 +1,59 @@
+var bob = {
+  firstName: "Bob",
+  lastName: "Jones",
+  phoneNumber: "(650) 777-7777",
+  email: "bob.jones@example.com"
+};
+
+var mary = {
+  firstName: "Mary",
+  lastName: "Johnson",
+  phoneNumber: "(650) 888-8888",
+  email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+//console.log(mary.phoneNumber);
+
+//Displays people from the list
+function printPerson(person){
+  console.log(person.firstName + " " + person.lastName);
+}
+
+//printPerson(contacts[0]);
+//printPerson(contacts[1]);
+
+//Lists everyone in contact list
+function list(){
+  var contactsLength = contacts.length;
+  for (var i=0; i < contactsLength; i++) {
+    printPerson(contacts[i]);
+  }
+}
+
+//finding someone special
+function search(lastName) {
+  var contactsLength = contacts.length;
+    for (var i=0; i < contactsLength; i++) {
+      if (lastName == lastName){
+        printPerson(contacts[i]);
+      } else {
+        console.log("Sorry no name has been found");
+      };
+    };
+ }
+
+//Add new people to list
+function add(firstName, lastName, email, phoneNumber){
+  contacts[contacts.length] = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    phoneNumber: phoneNumber
+  }
+};
+
+add("Steve", "Jobs", "sjobs@example.com", "(555) 555-4949");
+search("Jones");
+list();
